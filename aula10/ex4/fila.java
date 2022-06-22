ffa8e6fe13649169cb6a2cf3b6d02874ea436ae7
Fellipe Lyra <84457207+FellipeLyra@users.noreply.github.com>
2022-06-22 20:09:46 -0300

    Create fila.java

---
+++ aula10/ex4/fila.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fila {
+    List<Integer> fila = new ArrayList<>();
+    
+    public void push(int numero){
+        fila.add(numero);
+    }
+    
+    public int pop(){
+        int removido = fila.get(0);
+        fila.remove(0);
+        return removido;
+    }
+    
+    public boolean empty(){
+        return fila.isEmpty();
+    }
+    
+    public int tamanhoFila(){
+        return fila.size();
+    }
+}
