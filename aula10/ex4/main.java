46e08b7752c79d4ebf8da0dc9c937d973586560e
Fellipe Lyra <84457207+FellipeLyra@users.noreply.github.com>
2022-06-22 20:10:05 -0300

    Create main.java

---
+++ aula10/ex4/main.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Stack;
+
+public class exc4 {
+    public static void main(String[] args) {
+        Fila F1 = new Fila();
+        Stack<Integer> pilha = new Stack<>();
+        Fila F2 = new Fila();
+        
+        Random random = new Random();
+        
+        byte i=0;
+        
+        while(F1.tamanhoFila() < 100){
+            F1.push(random.nextInt(100)); 
+        }
+        
+        while(!F1.empty()){
+            pilha.push(F1.pop());
+            F2.push(pilha.pop());
+        }
+        
+        while(!F2.empty()){
+            System.out.println(i + ". "+ F2.pop());
+            i++;
+        }
+    }
+}
